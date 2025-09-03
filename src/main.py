874dd5b1ec6fa47b7c3d2075b725ba3238ed8b73
foxerBN <barspin4499@gmail.com>
2025-09-03 16:53:05 +0200

    static 404 not found page

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from src.routes.api import process_api
 
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="../templates")
 
 
 app.register_blueprint(process_api,url_prefix='/api')
@@ -16,11 +16,7 @@ def test():
 
 @app.errorhandler(404)
 def not_found(e):
-    return jsonify({
-        "error": "Not Found",
-        "message": "This route probably does not exist."
-    }), 404
-
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
